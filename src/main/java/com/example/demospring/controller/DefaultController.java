2b8d3899398bcf87d8a7f7bbcfd0adadb5f1e037
luanvo2911 <luanvovan2911@gmail.com>
2024-03-18 17:12:56 +0700

    first commit

---
+++ src/main/java/com/example/demospring/controller/DefaultController.java
@@ -0,0 +1,15 @@
+package com.example.demospring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class DefaultController {
+  @GetMapping("/")
+    public String homepage() {
+        return "Hello, this is homepage";
+    }
+  
+}
