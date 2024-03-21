8ff924bfce67b5b0731401bfe12e72778ccda2af
luanvo2911 <luanvovan2911@gmail.com>
2024-03-21 16:50:20 +0700

    fix CORS

--- src/main/java/com/example/demospring/controller/TodoController.java
+++ src/main/java/com/example/demospring/controller/TodoController.java
@@ -1,5 +1,6 @@
 package com.example.demospring.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.example.demospring.service.TodoService;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"})
 @RequestMapping ("/todos")
 public class TodoController {
 
--- src/main/java/com/example/demospring/controller/UserController.java
+++ src/main/java/com/example/demospring/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demospring.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.example.demospring.model.User;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"})
 @RequestMapping("/users")
 public class UserController {
 
